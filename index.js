51dd6ee0273b942d77e29a0ec77b897f9bfa0dc9
anilsebastin <anilsebastin@gmail.com>
2023-10-21 08:02:35 +0530

    Created MVC / integrated w Product, Brand, Categories

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const { createProduct } = require("./controller/Product");
+const productsRouter = require("./routes/Products");
+const categoriesRouter = require("./routes/Categories");
+const brandsRouter = require("./routes/Brands");
+
+// mongoose live db link
+// mongodb+srv://anilsebastin:<password>@cluster0.2jgtikz.mongodb.net/
+
+// Middlewares
+server.use(
+  cors({
+    exposedHeaders: ["X-Total-Count"],
+  })
+);
+server.use(express.json()); // to parse req.body
+server.use("/products", productsRouter.router);
+server.use("/categories", categoriesRouter.router);
+server.use("/brands", brandsRouter.router);
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/mern_ecommerce");
+  console.log("database connected");
+}
+
+server.get("/", (req, res) => {
+  res.json({ status: "success" });
+});
+
+server.post("/products", createProduct);
+
+server.listen(8080, () => {
+  console.log("server started");
+});
